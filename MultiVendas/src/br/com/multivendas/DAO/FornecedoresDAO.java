f76871bfd291cce35d4390fad45fbfb684182ce4
Andre Franklin <Andre Franklin@Notebook>
2019-08-16 00:08:58 -0300

    Primeiro commit para enviar para o servidor

---
+++ MultiVendas/src/br/com/multivendas/DAO/FornecedoresDAO.java
@@ -0,0 +1,53 @@
+package br.com.multivendas.DAO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import br.com.multivendas.conexao.Conexao_bd;
+import br.com.multivendas.domain.Fornecedores;
+
+public class FornecedoresDAO {
+
+	public void salvar(Fornecedores f) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+
+		sql.append("INSERT INTO fornecedores ");
+		sql.append("(descricao_forn)");
+		sql.append("VALUES (?)");
+
+		Connection conexao = Conexao_bd.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, f.getDescricao());
+		comando.executeUpdate();
+	}
+
+	public void excluir(Fornecedores f) throws SQLException {
+
+		StringBuilder sql = new StringBuilder();
+		sql.append("DELETE FROM fornecedores ");
+		sql.append("WHERE codigo_forn = ?");
+
+		Connection conexao = Conexao_bd.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setLong(1, f.getCodigo());
+		comando.executeUpdate();
+	}
+
+	public void editar(Fornecedores f) throws SQLException {
+
+		StringBuilder sql = new StringBuilder();
+		sql.append("UPDATE fornecedores ");
+		sql.append("SET descricao_forn = ? ");
+		sql.append("WHERE codigo_forn = ?");
+
+		Connection conexao = Conexao_bd.conectar();
+
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, f.getDescricao());
+		comando.setLong(2, f.getCodigo());
+		comando.executeUpdate();
+	}
+
+}
---
+++ MultiVendas/src/br/com/multivendas/conexao/Conexao_bd.java
@@ -0,0 +1,31 @@
+package br.com.multivendas.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
+public class Conexao_bd {
+
+	private static final String USUARIO = "root";
+	private static final String SENHA = "";
+	private static final String URL = "jdbc:mysql://localhost:3306/bd_multivendas";
+
+	public static Connection conectar() throws SQLException {
+		Connection conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
+		return conexao;
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			Connection conexao = Conexao_bd.conectar();
+			System.out.println("Conexão com Banco de dados realizada com sucesso !");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			System.out.println("Conexão com o banco de dados falhou !");
+		}
+
+	}
+
+}
